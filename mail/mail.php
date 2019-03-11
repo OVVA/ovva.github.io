8f8e12a415a13e6044554b1f1b7960940b5fc2cf
Volodymyr Prykhodko <fliros@windowslive.com>
2019-03-11 11:11:43 +0100

    1.2.06
    mail form config

--- mail/mail.php
+++ mail/mail.php
@@ -10,39 +10,29 @@ $emailMethod                = 'smtp'; // REQUIRED value. Options: 'smtp' , 'phpm
 
 // Outgoing Server Settings - replace values on the right of the = sign with your own.
 // These 3 settings are only required if you choose 'smtp' for emailMethod above.
-$outgoingServerAddress      = 'mail.yourdomain.com'; // Consult your hosting provider.
-$outgoingServerPort         = '25';                  // Options: '587' , '25' - Consult your hosting provider.
-$outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
+$outgoingServerAddress      = 'smtp.zoho.com';      // Consult your hosting provider.
+$outgoingServerPort         = '465';                // Options: '587' , '25' - Consult your hosting provider.
+$outgoingServerSecurity     = 'ssl';                // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'email@domain.com';
-$sendingAccountPassword     = 'p@55w0rd';
+$sendingAccountUsername     = 'webmaster@ovva.sk';
+$sendingAccountPassword     = 'Fliros28!';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
-$recipientName              = 'John Doe';             // REQUIRED value.
+$recipientEmail             = 'webmaster@ovva.sk'; // REQUIRED value.
+$recipientName              = 'WebMaster OVVA.sk'; // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
-$emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
-$websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
+$emailSubject               = 'A message from OVVA.sk Website'; // REQUIRED value. Subject of the email that the recipient will see
+$websiteName                = 'OVVA - Websolutions for business';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone                   = 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
+$timeZone                   = 'Slovakia/Bratislava';         // OPTIONAL, but some servers require this to be set. 
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
 
 // Google reCAPTCHA
 $recaptchaSecretKey         = '18cb38cwucb73vo708r6vgshjcbwbfbcvueucbwg';
 $recaptchaErrorMessage      = 'There was a problem verifying the Google reCaptcha.  Please try again.';
-
-// Confirmation Message
-$sendConfirmationToUser     = false; // leave false to disable confirmation, or use path to an html email template.
-$confirmationSubject        = "Thanks for Contacting Company Name"; // The subject of the confirmation email
-$confirmationFromName       = "Company Name"; // Used in the "from" field of the email.
-$userEmailField             = "email";
-$defaultUserName            = "User"; // Used if the form does not include a "name" field.
-
-// CSV
-$saveToCSV                  = "email_form.csv";
 
  /*----------------------------------------------------------------------------*\
 |*  You do not need to edit anything below this line, the rest is automatic.    *|
